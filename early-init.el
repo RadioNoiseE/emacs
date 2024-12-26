79fa79680f09b7f6deffa5c5f7c101b06e64054b
RadioNoiseE <j18516785606@icloud.com>
2024-12-26 16:29:51 +0800

    ns-transparent-titlebar enabled

--- early-init.el
+++ early-init.el
@@ -14,11 +14,11 @@
       process-adaptive-read-buffering nil
       frame-inhibit-implied-resize t
       frame-resize-pixelwise t
-      default-frame-alist '((ns-transparent-titlebar . nil)
+      default-frame-alist '((width . 150)
+                            (height . 50)
                             (vertical-scroll-bar . nil)
                             (horizontal-scroll-bar . nil)
-                            (width . 150)
-                            (height . 50)))
+                            (ns-transparent-titlebar t)))
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
